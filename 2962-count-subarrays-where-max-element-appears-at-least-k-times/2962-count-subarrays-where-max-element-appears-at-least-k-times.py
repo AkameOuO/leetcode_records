0c1544bac1ff7b818c5380a01c043329d691294d
AkameOuO <30804171+AkameOuO@users.noreply.github.com>
2024-03-30 01:21:03 +0900

    Time: 975 ms (25.19%), Space: 31.1 MB (38.93%) - LeetHub

--- 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.py
+++ 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.py
@@ -1,15 +1,17 @@
 class Solution:
     def countSubarrays(self, nums: List[int], k: int) -> int:
-        t = max(nums)
-        l = list(filter(lambda x:nums[x] == t,range(len(nums))))
-        if len(l) < k:
+        l = [-1]
+        m = 0
+        for i,n in zip(count(), nums):
+            if m < n:
+                m = n
+                l = [-1,i]
+            elif m == n:
+                l.append(i)
+        if len(l) <= k:
             return 0
         res = 0
-        last = -1
-        for i in range(len(l)-k+1):
-            # s = l[i:i+k]
-            res += (l[i] - last) * (len(nums) - l[i+k-1])
-            last = l[i]
-            # print(s,l[i] - last,len(nums) - l[i+k-1],res)
-        print(l)
+        for i in range(1,len(l) - k+1):
+            res += (l[i] - l[i-1]) * (len(nums) - l[i+k-1])
+            print(l[i],l[i] - l[i-1],len(nums) - l[i+k-1])
         return res
