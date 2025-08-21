0bbb364539994269ebd33a88005edc29fb2d3474
AkameOuO <akame@akameouo.dev>
2025-08-21 18:09:55 +0900

    Time: 53 ms (65.11%), Space: 19.1 MB (5.3%) - LeetHub

--- 1628-count-submatrices-with-all-ones/1628-count-submatrices-with-all-ones.py
+++ 1628-count-submatrices-with-all-ones/1628-count-submatrices-with-all-ones.py
@@ -1,7 +1,5 @@
 class Solution:
     def numSubmat(self, mat: List[List[int]]) -> int:
-        def print(*awgs): pass
-        
         mat2 = []
         for row in mat:
             row2 = []
@@ -11,7 +9,7 @@ class Solution:
                 row2.append(cur)
             mat2.append(row2)
         mat2 = list(map(list, zip(*mat2)))
-        print(mat2)
+
         res = 0
         for row in mat2:
             stack = []
@@ -20,21 +18,16 @@ class Solution:
                     stack = []
                     continue
                 
-                stack.append([c, i])
+                stack.append([c, i - 1])
                 while len(stack) > 1:
                     if stack[-2][0] < c:
                         break
                     stack[-2][0] = c
                     stack.pop()
 
-                print(stack)
                 for sc, si in stack[::-1]:
-                    print("-", sc * (i - si + 1))
-                    res += sc * (i - si + 1)
-                    i = si - 1
-
-                print(res)
-            print("="*10)
+                    res += sc * (i - si)
+                    i = si
 
         return res
                 
